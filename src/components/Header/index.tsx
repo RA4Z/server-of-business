2f62ec7c2f919db2a01d528a7530d4b8a35374a1
RA4Z <rraz639@gmail.com>
2023-11-07 11:13:44 -0300

    Adicionando estilização na barra header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,15 +3,44 @@ import Button from '../Button'
 import styles from './Header.module.scss'
 
 export default function Header() {
+    const rotas = [{
+        label: 'Contratar',
+        to: '/'
+    }, {
+        label: 'Candidatar-se',
+        to: '/'
+    }];
+
+    const menu_item = [{
+        label: 'Login',
+        dark: false,
+        to: '/'
+    }, {
+        label: 'Cadastre-se',
+        dark: true,
+        to: '/'
+    }]
     return (
         <div className={styles.header}>
             <img className={styles.img} src={logo} alt='Logo do projeto' />
-            <div>
-                <p>Contratar</p>
-                <p>Candidatar-se</p>
-            </div>
-            <Button texto={'Login'} dark={false} />
-            <Button texto={'Cadastre-se'} dark={true} />
+
+            <ul className={styles.menu__list}>
+                {rotas.map((rota, index) => (
+                    <li key={index} className={styles.menu__link}>
+                        <a href={rota.to}>{rota.label}</a>
+                    </li>
+                ))}
+            </ul>
+            <ul className={styles.menu__list}>
+                {menu_item.map((menu_item, index) => (
+                    <li key={index} className={styles.menu__button}>
+                        <a href={menu_item.to}>
+                            <Button texto={menu_item.label} dark={menu_item.dark} />
+                        </a>
+                    </li>
+                ))}
+            </ul>
+
         </div>
     )
 }
