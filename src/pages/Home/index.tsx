68e22f9894678d577327eeb93eae0f0713cd9e7c
RA4Z <rraz639@gmail.com>
2023-11-08 17:04:05 -0300

    Preparando apresentação dos cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,37 @@
 import Card from 'components/Card';
 import Header from 'components/Header';
+import styles from './Home.module.scss';
 import 'pages/home.css';
 
 export default function Home() {
+    const cards_especialista = [
+        {
+            titulo: 'Prestar serviço como Freelancer',
+            descricao: 'Procurar serviços de curta duração, para serem realizados em poucos dias ou horas.',
+            imagem: '/assets/freelancer.jpg',
+            buttonText: 'Candidatar-se como Freelancer',
+        },
+        {
+            titulo: 'Prestar serviço como Autônomo',
+            descricao: 'Procurar oportunidades de serviços de médio a longo prazo para duração de meses a anos.',
+            imagem: '/assets/autonomo.jpg',
+            buttonText: 'Candidatar-se como Autônomo',
+        }
+    ]
     return (
         <div>
             <Header logado={false} selected={0} />
-            <div className="App-body">
-                <Card
-                    titulo='Prestar serviço como Freelancer'
-                    descricao='Procurar serviços de curta duração, para serem realizados em poucos dias ou horas.'
-                    imagem='/assets/freelancer.jpg'
-                    buttonText='Candidatar-se como Freelancer'
-                />
+            <div className={styles.container}>
+                <div className={styles.cards}>
+                    {cards_especialista.map((card) => (
+                        <Card
+                            titulo={card.titulo}
+                            descricao={card.descricao}
+                            imagem={card.imagem}
+                            buttonText={card.buttonText}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
