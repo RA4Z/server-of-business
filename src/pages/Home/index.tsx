0f3dad6b0fa96791549dc3784f716ed3d936a3f8
RA4Z <rraz639@gmail.com>
2023-11-09 14:46:22 -0300

    Criando os blocos de propaganda

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import contratante from 'images/main_contratante.jpg';
+
 import Card from 'components/Card';
 import Header from 'components/Header';
 import styles from './Home.module.scss';
@@ -33,8 +35,17 @@ export default function Home() {
     }
 
     return (
-        <div onClick={event => event}>
-            <Header logado={false} selected={0} />
+        <div>
+            <Header logado={false} selected={1} />
+
+            <div className={styles.propaganda}>
+                <div>
+                    <p className={styles.propaganda__title}>Todo dia precisamos resolver diversos problemas, por que não procurar alguém especializado na resolução deles?</p>
+                    <p className={styles.propaganda__description}>Aqui você pode encontrar especialistas das mais diversas áreas de negócio que podem resolver dos mais diversos tipos de problemas em sua vida!</p>
+                </div>
+                <img src={contratante} alt='Pessoas apertando as mãos' />
+            </div>
+
             <div className={styles.container}>
                 <div className={styles.cards}>
                     {cards_especialista.map((card) => (
