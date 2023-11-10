2e12aadd9e87f043db0300fabc52d98be4e1a611
RA4Z <rraz639@gmail.com>
2023-11-10 13:00:04 -0300

    Adicionando Footer no projeto

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Footer.module.scss'
+
+export default function Footer() {
+    return (
+        <div className={styles.background}>
+            <text>Server of Business</text>
+            <text>Desenvolvido e prototipado por Robert Aron Zimmermann</text>
+        </div>
+    )
+}
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Header from 'components/Header';
 import { useState } from 'react';
+import Footer from 'components/Footer';
 
 export default function AppRouter() {
     const [pagina, setPagina] = useState(0)
@@ -16,6 +17,7 @@ export default function AppRouter() {
             <Routes>
                 <Route path='/' element={<Home pagina={pagina} childToParent={childToParent} />} />
             </Routes>
+            <Footer />
         </Router>
     )
 }
