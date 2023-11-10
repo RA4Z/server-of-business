effe5a30d7876441cc1013cf9a06c4f917e023f8
RA4Z <rraz639@gmail.com>
2023-11-10 15:24:35 -0300

    Reduzindo regras de caminhos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,15 +10,7 @@ import OptionMenu from 'components/OptionMenu'
 export default function Header({ logado = false, selected = 0, childToParent }: any) {
     const [option, setOption] = useState(false)
 
-    const rotas_logon = [{
-        label: 'Especialistas',
-        selected: 1,
-    }, {
-        label: 'Serviços',
-        selected: 2
-    }];
-
-    const rotas_logoff = [{
+    const rotas = [{
         label: 'Contratar',
         selected: 1,
     }, {
@@ -41,18 +33,18 @@ export default function Header({ logado = false, selected = 0, childToParent }: 
             <div className={styles.header}>
                 <img className={styles.img} src={logo} alt='Logo do projeto' />
 
-                {logado ?
-                    <>
-                        <ul className={styles.menu__list}>
-                            {rotas_logon.map((rotas_logon, index) => (
+                <ul className={styles.menu__list}>
+                            {rotas.map((rotas, index) => (
                                 <li key={index} className={styles.menu__link}>
-                                    {selected === rotas_logon.selected ?
-                                        <p>{rotas_logon.label}</p>
+                                    {selected === rotas.selected ?
+                                        <p>{rotas.label}</p>
                                         :
-                                        <button onClick={() => childToParent(rotas_logon.selected - 1)}>{rotas_logon.label}</button>}
+                                        <button onClick={() => childToParent(rotas.selected - 1)}>{rotas.label}</button>}
                                 </li>
                             ))}
                         </ul>
+                {logado ?
+                    <>
                         <ul className={styles.menu__buttons}>
                             <li className={styles.menu__button}>
                                 <img className={styles.user} src={user} alt='Perfil de usuário' onClick={() => setOption(!option)} />
@@ -61,16 +53,6 @@ export default function Header({ logado = false, selected = 0, childToParent }: 
                     </>
                     :
                     <>
-                        <ul className={styles.menu__list}>
-                            {rotas_logoff.map((rotas_logoff, index) => (
-                                <li key={index} className={styles.menu__link}>
-                                    {selected === rotas_logoff.selected ?
-                                        <p>{rotas_logoff.label}</p>
-                                        :
-                                        <button onClick={() => childToParent(rotas_logoff.selected - 1)}>{rotas_logoff.label}</button>}
-                                </li>
-                            ))}
-                        </ul>
                         <ul className={styles.menu__buttons}>
                             {menu_item.map((menu_item, index) => (
                                 <li key={index} className={styles.menu__button}>
