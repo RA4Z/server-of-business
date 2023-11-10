e57517175a9f03c7d4211366f04c826512d73bf2
RA4Z <rraz639@gmail.com>
2023-11-10 11:01:47 -0300

    Criação de rotas

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
+import AppRouter from 'pages/routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    <AppRouter />
   </React.StrictMode>
 );
