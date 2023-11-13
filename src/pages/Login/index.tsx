6e1eeec780a28b312421837daba8fb78356b6afd
RA4Z <rraz639@gmail.com>
2023-11-13 10:04:13 -0300

    Criando estilos básicos para a tela de Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,14 @@
+import Logo from 'images/logo.svg'
+import styles from './Login.module.scss';
+
 export default function Login() {
+
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <div className={styles.container}>
+                <img src={Logo} alt='Logotipo da empresa' />
+                <p className={styles.container__title}>Server of Business</p>
+            </div>
+        </>
     )
 } 
