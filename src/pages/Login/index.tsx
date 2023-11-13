ae5b5a6efb0dfec8b1a6c8dba96aef25d4476ded
RA4Z <rraz639@gmail.com>
2023-11-13 10:43:49 -0300

    Estilizando a tela de Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,29 @@
 import Logo from 'images/logo.svg'
 import styles from './Login.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { TextField } from '@mui/material';
+import Button from 'components/Button';
 
 export default function Login() {
-
+    const navigate = useNavigate();
+    
     return (
         <>
             <div className={styles.container}>
-                <img src={Logo} alt='Logotipo da empresa' />
+                <img src={Logo} onClick={() => navigate('/')} alt='Logotipo da empresa' />
                 <p className={styles.container__title}>Server of Business</p>
+                <form className={styles.container__inputs}>
+                    <TextField id="outlined-basic" label="E-mail" variant="outlined" style={{ width: 500 }} />
+                    <TextField id="outlined-password-input" label="Senha" variant="outlined" type='password' autoComplete="current-password" style={{ width: 500 }} />
+                </form>
+                <p className={styles.container__password}>Esqueceu sua senha?</p>
+                <Button texto='Login' dark={true} />
+
+                <div className={styles.container__criar}>
+                    <p>Ainda não possui uma conta?</p>
+                    <button>Criar conta</button>
+                </div>
+
             </div>
         </>
     )
