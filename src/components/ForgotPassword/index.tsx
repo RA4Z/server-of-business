ccebe7cfd09e07d6cb613304f9862616a978916e
RA4Z <rraz639@gmail.com>
2023-11-14 10:09:13 -0300

    Permitindo abrir e fechar o bloco de redefinição de senha

--- src/components/ForgotPassword/index.tsx
+++ src/components/ForgotPassword/index.tsx
@@ -3,17 +3,24 @@ import styles from './ForgotPassword.module.scss'
 import Logo from 'images/logo.svg'
 import Button from 'components/Button'
 
-export default function ForgotPassword() {
+export default function ForgotPassword({ visible, childToParent }: any) {
     return (
-        <div className={styles.container}>
-            <img src={Logo} alt='Logotipo Server of Business' />
-            <p>Insira seu E-mail abaixo</p>
-            <TextField id="outlined-basic" label="E-mail" variant="outlined" className={styles.input} />
-            <p>Iremos enviar o protocolo de redefinição de senha</p>
-            <div className={styles.container__buttons}>
-                <Button dark={false} texto='Cancelar' />
-                <Button dark={true} texto='Enviar Protocolo de Redefinição' />
-            </div>
-        </div>
+        <>
+            {visible ?
+                <>
+                    <div className={styles.overlay} onClick={() => childToParent(false)} />
+                    <div className={styles.container}>
+                        <img src={Logo} alt='Logotipo Server of Business' />
+                        <p>Insira seu E-mail abaixo</p>
+                        <TextField id="outlined-basic" label="E-mail" variant="outlined" autoComplete="email" className={styles.input} />
+                        <p>Iremos enviar o protocolo de redefinição de senha</p>
+                        <div className={styles.container__buttons}>
+                            <Button dark={false} texto='Cancelar' onClick={() => childToParent(false)} />
+                            <Button dark={true} texto='Enviar Protocolo de Redefinição' />
+                        </div>
+                    </div>
+                </>
+                : ''}
+        </>
     )
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,27 +3,33 @@ import styles from './Login.module.scss';
 import { useNavigate } from 'react-router-dom';
 import { TextField } from '@mui/material';
 import Button from 'components/Button';
+import ForgotPassword from 'components/ForgotPassword';
+import { useState } from 'react';
 
 export default function Login() {
+    const [forgotPassWordVisible, setForgotPassWordVisible] = useState(false)
     const navigate = useNavigate();
 
+    const childToParent = (childdata: boolean) => {
+        setForgotPassWordVisible(childdata)
+    }
     return (
         <>
             <div className={styles.container}>
                 <img src={Logo} onClick={() => navigate('/')} alt='Logotipo da empresa' />
                 <p className={styles.container__title}>Server of Business</p>
                 <form className={styles.container__inputs}>
-                    <TextField id="outlined-basic" label="E-mail" variant="outlined" className={styles.input} />
+                    <TextField id="outlined-basic" label="E-mail" variant="outlined" autoComplete="email" className={styles.input} />
                     <TextField id="outlined-password-input" label="Senha" variant="outlined" type='password' autoComplete="current-password" className={styles.input} />
                 </form>
-                <p className={styles.container__password}>Esqueceu sua senha?</p>
+                <p className={styles.container__password} onClick={() => setForgotPassWordVisible(true)}>Esqueceu sua senha?</p>
                 <Button texto='Login' dark={true} />
 
                 <div className={styles.container__criar}>
                     <p>Ainda não possui uma conta?</p>
                     <button onClick={() => navigate('/cadastro')}>Criar conta</button>
                 </div>
-
+                <ForgotPassword visible={forgotPassWordVisible} childToParent={childToParent} />
             </div>
         </>
     )
