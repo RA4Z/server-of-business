dc3597c9da86b2f3af5a00008d6f880dd2d67e60
RA4Z <rraz639@gmail.com>
2023-11-14 15:38:10 -0300

    Exibindo cards com valores estáticos

--- src/pages/Pesquisa/index.tsx
+++ src/pages/Pesquisa/index.tsx
@@ -1,7 +1,9 @@
 import { Checkbox, Divider, FormControlLabel, TextField } from '@mui/material'
+import { info_preenchida } from './infos'
 import styles from './Pesquisa.module.scss'
 import Button from 'components/Button'
 import { useParams } from 'react-router-dom'
+import Card from 'components/Card'
 
 export default function Pesquisa({ childToParent }: any) {
     const { categoria, especifico } = useParams();
@@ -21,7 +23,7 @@ export default function Pesquisa({ childToParent }: any) {
             <form>
                 <div className={styles.pesquisas}>
                     <div className={styles.pesquisas__left}>
-                        <TextField id="outlined-name" label="Nome Especialista" variant="outlined" autoComplete="username" className={styles.input} />
+                        <TextField id="outlined-name" label={info_preenchida[Number(categoria) - 1].titulo} variant="outlined" autoComplete="username" className={styles.input} />
                         <div>
                             <FormControlLabel control={<Checkbox defaultChecked={especifico === 'free' ? true : false} />} label="Freelancer" className={styles.check} />
                             <FormControlLabel control={<Checkbox defaultChecked={especifico === 'auto' ? true : false} />} label="Autônomo" className={styles.check} />
@@ -36,6 +38,17 @@ export default function Pesquisa({ childToParent }: any) {
                 <Divider style={{ background: '#7C7C7C', width: 'auto', margin: 25 }}></Divider>
             </form>
 
+            <div className={styles.container}>
+                {info_preenchida[Number(categoria) - 1].cards.map((card) => (
+                    <Card
+                        key={card.titulo}
+                        titulo={card.titulo}
+                        descricao={card.descricao}
+                        imagem={card.imagem}
+                        buttonText='Ver mais informações'
+                    />
+                ))}
+            </div>
         </>
     )
 }
---
+++ src/pages/Pesquisa/infos.ts
@@ -0,0 +1,73 @@
+export const info_preenchida = [
+    {
+        pagina: 'Especialistas',
+        titulo: 'Nome Especialista',
+        cards: [
+            {
+                titulo: 'Jotaro Kujo',
+                estrelas: 4.7,
+                cargo: 'Biólogo',
+                descricao: 'Ser humano normal, usuário do Star Platinum, delinquente e assassino de Dio Brando, membro principal da gangue dos Crusaders, biólogo ...',
+                imagem: '/assets/user.png',
+            },
+            {
+                titulo: 'Jotaro Kujo',
+                estrelas: 4.7,
+                cargo: 'Biólogo',
+                descricao: 'Ser humano normal, usuário do Star Platinum, delinquente e assassino de Dio Brando, membro principal da gangue dos Crusaders, biólogo ...',
+                imagem: '/assets/user.png',
+            },
+            {
+                titulo: 'Jotaro Kujo',
+                estrelas: 4.7,
+                cargo: 'Biólogo',
+                descricao: 'Ser humano normal, usuário do Star Platinum, delinquente e assassino de Dio Brando, membro principal da gangue dos Crusaders, biólogo ...',
+                imagem: '/assets/user.png',
+            },
+            {
+                titulo: 'Jotaro Kujo',
+                estrelas: 4.7,
+                cargo: 'Biólogo',
+                descricao: 'Ser humano normal, usuário do Star Platinum, delinquente e assassino de Dio Brando, membro principal da gangue dos Crusaders, biólogo ...',
+                imagem: '/assets/user.png',
+            },
+            {
+                titulo: 'Jotaro Kujo',
+                estrelas: 4.7,
+                cargo: 'Biólogo',
+                descricao: 'Ser humano normal, usuário do Star Platinum, delinquente e assassino de Dio Brando, membro principal da gangue dos Crusaders, biólogo ...',
+                imagem: '/assets/user.png',
+            },
+        ]
+    },
+    {
+        pagina: 'Serviços',
+        titulo: 'Título do Serviço',
+        cards: [
+            {
+                titulo: 'Pedreiro Solicitado em Schroeder',
+                necessario: 'Autônomo',
+                descricao: 'Obra em Schroeder na rua Germano Muller, bairro Centro Norte, expectativa de 6 meses de obra.',
+                imagem: '/assets/contratar_freelancer.jpg',
+            },
+            {
+                titulo: 'Pedreiro Solicitado em Schroeder',
+                necessario: 'Autônomo',
+                descricao: 'Obra em Schroeder na rua Germano Muller, bairro Centro Norte, expectativa de 6 meses de obra.',
+                imagem: '/assets/contratar_freelancer.jpg',
+            },
+            {
+                titulo: 'Pedreiro Solicitado em Schroeder',
+                necessario: 'Autônomo',
+                descricao: 'Obra em Schroeder na rua Germano Muller, bairro Centro Norte, expectativa de 6 meses de obra.',
+                imagem: '/assets/contratar_freelancer.jpg',
+            },
+            {
+                titulo: 'Pedreiro Solicitado em Schroeder',
+                necessario: 'Autônomo',
+                descricao: 'Obra em Schroeder na rua Germano Muller, bairro Centro Norte, expectativa de 6 meses de obra.',
+                imagem: '/assets/contratar_freelancer.jpg',
+            }
+        ]
+    }
+]
