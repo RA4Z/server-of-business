b872e3567fca8e13249120f88e0905dde9d6b860
RA4Z <rraz639@gmail.com>
2023-11-16 09:49:46 -0300

    Estilização básica nos serviços

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -1,12 +1,28 @@
+import Button from 'components/Button';
 import styles from './Info.module.scss'
+
+import Voltar from 'images/voltar.png'
+import Obra from 'images/obra-temp.png'
+
 import { useParams } from 'react-router-dom'
 
-export default function Info({ childToParent }:any) {
+export default function Info({ childToParent }: any) {
     const { categoria, id } = useParams();
-    
+
     return (
-        <div>
-            Informações sobre o selecionado {id}...
+        <div className={styles.container}>
+            <img src={Voltar} alt='Seta para retornar à página anterior' className={styles.seta_volta} />
+            <div className={styles.info}>
+                <div className={styles.info__titulo} >Pedreiro solicitado em Schroeder</div>
+                <div className={styles.info__desc}>
+                    <img src={Obra} alt='Imagem do serviço solicitado' />
+                    <p>Obra em Schroeder na rua Germano Muller, bairro Centro Norte, expectativa de 6 meses de obra.
+                        Registrado para início em 27/01/2024 às 08:00.
+                        Trabalho para autônomos
+                        Trabalho solicitado por Robert Aron Zimmermann.</p>
+                </div>
+            </div>
+            <Button texto='Candidatar-se ao serviço' dark={true} />
         </div>
     )
 }
