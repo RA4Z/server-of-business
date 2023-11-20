c5c998a7ea1e4162e04b4b21601007826d9b9b9a
RA4Z <rraz639@gmail.com>
2023-11-20 09:32:32 -0300

    Começando estilização da página de perfil de usuário

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,7 +1,28 @@
+import UserImg from 'images/user.png'
+import Calendario from 'images/calendario.jpg'
+
+import Card from 'components/Card'
+
+import styles from './Perfil.module.scss'
+
 export default function Perfil() {
     return (
-        <div>
-            Perfil de usuário
+        <div className={styles.container}>
+            <div className={styles.user}>
+                <img src={UserImg} alt='Imagem do perfil de usuário' />
+                <p className={styles.user__editar}>Editar perfil</p>
+            </div>
+            <div>
+                <p>Jotaro Kujo</p>
+                <p>4.7 Biólogo</p>
+                <p>Schroeder, Santa Catarina, Brazil</p>
+            </div>
+            <Card
+                buttonText='Abrir solicitação de serviço'
+                titulo='Gostaria de agendar uma solicitação?'
+                descricao='Abra um chamado em algum horário para qualquer categoria de serviço, diversas pessoas de sua região serão notificadas para atendimento.'
+                imagem={Calendario}
+            />
         </div>
     )
 }
