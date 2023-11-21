3db2dc455809defcdee80a4211d70663e8be435f
RA4Z <rraz639@gmail.com>
2023-11-21 15:42:17 -0300

    Removendo Especializações de dentro de um array

--- src/pages/Perfil/Adicionar/index.tsx
+++ src/pages/Perfil/Adicionar/index.tsx
@@ -1,9 +1,16 @@
 import { Divider, TextField } from '@mui/material'
+import { useState } from 'react'
 import styles from './Adicionar.module.scss'
 import Cancelar from 'images/cancelar.png'
 import Button from 'components/Button'
 
 export default function Adicionar({ visible }: any) {
+    const [especializacoes, setEspecializacoes] = useState(['Biólogo', 'Engenheiro de software', 'Stand User'])
+
+    function removerEspecializacao(especializacao:string) {
+        setEspecializacoes(especializacoes.filter(especial => especial !== especializacao))
+    }
+
     return (
         <>
             <div className={styles.overlay} onClick={() => visible(false)} />
@@ -14,14 +21,12 @@ export default function Adicionar({ visible }: any) {
                 <Divider style={{ margin: 20 }} />
 
                 <div className={styles.especializacoes}>
-                    <div className={styles.especializacoes__especializacao}>
-                        <li>Biólogo</li>
-                        <img src={Cancelar} alt='Botão de cancelar' />
-                    </div>
-                    <div className={styles.especializacoes__especializacao}>
-                        <li>Engenheiro de software</li>
-                        <img src={Cancelar} alt='Botão de cancelar' />
-                    </div>
+                    {especializacoes.map((especializacao) => (
+                        <div className={styles.especializacoes__especializacao} key={especializacao}>
+                            <li>{especializacao}</li>
+                            <img src={Cancelar} alt='Botão de cancelar' onClick={() => removerEspecializacao(especializacao)} />
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
