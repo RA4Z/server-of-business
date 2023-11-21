315bfbee7882fbb1e66072f159df2514e6decb93
RA4Z <rraz639@gmail.com>
2023-11-21 16:19:48 -0300

    Função de Adicionar Especialização

--- src/pages/Perfil/Adicionar/index.tsx
+++ src/pages/Perfil/Adicionar/index.tsx
@@ -5,10 +5,15 @@ import Cancelar from 'images/cancelar.png'
 import Button from 'components/Button'
 
 export default function Adicionar({ visible }: any) {
+    const [texto, setTexto] = useState('')
     const [especializacoes, setEspecializacoes] = useState(['Biólogo', 'Engenheiro de software', 'Stand User'])
 
-    function removerEspecializacao(especializacao:string) {
+    function removerEspecializacao(especializacao: string) {
         setEspecializacoes(especializacoes.filter(especial => especial !== especializacao))
+    }
+    function adicionarEspecializacao(especializacao: string) {
+        especializacoes.push(especializacao)
+        setTexto('')
     }
 
     return (
@@ -16,8 +21,8 @@ export default function Adicionar({ visible }: any) {
             <div className={styles.overlay} onClick={() => visible(false)} />
             <div className={styles.container}>
                 <h2>Adição de Especializações</h2>
-                <TextField id="add-especialist" label="Nome da Especialização" variant="outlined" autoComplete="especialização" className={styles.input} />
-                <Button texto='Adicionar' dark={false} />
+                <TextField id="add-especialist" label="Nome da Especialização" value={texto} onChange={(e) => setTexto(e.target.value)} variant="outlined" autoComplete="especialização" className={styles.input} />
+                <Button texto='Adicionar' dark={false} onClick={() => adicionarEspecializacao(texto)} />
                 <Divider style={{ margin: 20 }} />
 
                 <div className={styles.especializacoes}>
