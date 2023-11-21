c77b0564a9368aade46dedb8e70fb984121d087d
RA4Z <rraz639@gmail.com>
2023-11-21 16:41:45 -0300

    Adicionando Overflow Adicionar dentro das solicitações de serviços

--- src/pages/Perfil/Solicitar/index.tsx
+++ src/pages/Perfil/Solicitar/index.tsx
@@ -1,13 +1,23 @@
 import { Checkbox, FormControlLabel, TextField } from '@mui/material'
+import { useState } from 'react'
 import UploadIco from 'images/upload.png'
 import styles from './Solicitar.module.scss'
 import Button from 'components/Button'
+import Adicionar from '../Adicionar'
 
 export default function Solicitar({ visible }: any) {
+    const [adicionar, setAdicionar] = useState(false)
+
+    const addEspecialista = (childdata: boolean) => {
+        setAdicionar(childdata)
+    }
     return (
         <>
             <div className={styles.overlay} onClick={() => visible(false)} />
             <div className={styles.container}>
+                {adicionar ? <div className={styles.bloco_servicos}>
+                    <Adicionar visible={addEspecialista} />
+                </div> : ''}
                 <div className={styles.lados}>
                     <div className={styles.left}>
                         <TextField id="outlined-solicitation-title" label="Título da solicitação" variant="outlined" autoComplete="title" className={styles.input} />
@@ -16,7 +26,7 @@ export default function Solicitar({ visible }: any) {
 
                     </div>
                     <div className={styles.right}>
-                        <Button texto='Possíveis Especializações' dark={false} />
+                        <Button texto='Possíveis Especializações' dark={false} onClick={() => setAdicionar(true)} />
                         <div className={styles.right__blocosdados}>
                             <TextField id="solicitation-date" label="Data" variant="outlined" autoComplete="date" className={styles.input__bloco} />
                             <TextField id="solicitation-time" label="Hora" variant="outlined" autoComplete="hour" className={styles.input__bloco} />
