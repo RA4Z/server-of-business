935a7e2447ed0d2eb22adccbdb0a289a4d9e6685
RA4Z <rraz639@gmail.com>
2023-11-21 09:43:16 -0300

    Criando botões de configurações de perfil de usuário

--- src/pages/Perfil/Editar/index.tsx
+++ src/pages/Perfil/Editar/index.tsx
@@ -1,10 +1,13 @@
-import { TextField } from '@mui/material'
-import { useState } from 'react'
+import { Checkbox, FormControlLabel, TextField } from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
 import styles from './Editar.module.scss'
 import { CountryDropdown, RegionDropdown } from 'react-country-region-selector'
 import Button from 'components/Button'
 
 export default function Editar({ visible }: any) {
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef);
+    const [showEspecialista, setShowEspecialista] = useState(false)
     const [country, setCountry] = useState('')
     const [region, setRegion] = useState('')
 
@@ -15,6 +18,20 @@ export default function Editar({ visible }: any) {
         document.body.style.overflow = "visible";
         visible(false)
     }
+    function useOutsideAlerter(ref: any) {
+        useEffect(() => {
+            function handleClickOutside(event: any) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    setShowEspecialista(false)
+                }
+            }
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
+
     return (
         <>
             <div className={styles.overlay} onClick={() => sair()} />
@@ -34,12 +51,25 @@ export default function Editar({ visible }: any) {
                         country={country}
                         value={region}
                         onChange={(val) => setRegion(val)} />
+                    <Button texto='Configurações Especialista' dark={false} onClick={() => setShowEspecialista(true)} />
                 </div>
                 <TextField id="outlined-multiline-static" label="Sobre você" multiline rows={4} variant="outlined" autoComplete="text" className={styles.input__about} />
+
                 <div className={styles.buttons}>
                     <Button texto='Cancelar' dark={false} onClick={() => sair()} />
                     <Button texto='Salvar' dark={true} />
                 </div>
+
+                {showEspecialista ? <div ref={wrapperRef} className={styles.bloco_servicos}>
+                    <TextField id="outlined-especialist" label="Especialista?" variant="outlined" autoComplete="yes/no" className={styles.bloco_servicos__input} />
+                    <Button dark={false} texto='Especializações' />
+                    <TextField id="outlined-hour-value" label="Valor hora" variant="outlined" autoComplete="money" className={styles.bloco_servicos__input} />
+                    <TextField id="outlined-exibition" label="Em exibição" variant="outlined" autoComplete="exibition" className={styles.bloco_servicos__input} />
+                    <>
+                        <FormControlLabel control={<Checkbox />} label="Autônomo" className={styles.check} />
+                        <FormControlLabel control={<Checkbox />} label="Freelancer" className={styles.check} />
+                    </>
+                </div> : ''}
             </div>
         </>
     )
