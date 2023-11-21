7b7ac3aa9a71dad74262c4f9c9f375f017f66192
RA4Z <rraz639@gmail.com>
2023-11-21 14:50:53 -0300

    Estilizando especializações adicionadas

--- src/pages/Perfil/Adicionar/index.tsx
+++ src/pages/Perfil/Adicionar/index.tsx
@@ -1,5 +1,6 @@
 import { Divider, TextField } from '@mui/material'
 import styles from './Adicionar.module.scss'
+import Cancelar from 'images/cancelar.png'
 import Button from 'components/Button'
 
 export default function Adicionar({ visible }: any) {
@@ -7,10 +8,21 @@ export default function Adicionar({ visible }: any) {
         <>
             <div className={styles.overlay} onClick={() => visible(false)} />
             <div className={styles.container}>
-                <h3>Adição de Especializações</h3>
+                <h2>Adição de Especializações</h2>
                 <TextField id="add-especialist" label="Nome da Especialização" variant="outlined" autoComplete="especialização" className={styles.input} />
                 <Button texto='Adicionar' dark={false} />
-                <Divider style={{margin:20}} />
+                <Divider style={{ margin: 20 }} />
+
+                <div className={styles.especializacoes}>
+                    <div className={styles.especializacoes__especializacao}>
+                        <li>Biólogo</li>
+                        <img src={Cancelar} alt='Botão de cancelar' />
+                    </div>
+                    <div className={styles.especializacoes__especializacao}>
+                        <li>Engenheiro de software</li>
+                        <img src={Cancelar} alt='Botão de cancelar' />
+                    </div>
+                </div>
             </div>
         </>
     )
