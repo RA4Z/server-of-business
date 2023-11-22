7ede0398901e3bc6e0ce5d9b2ad04b38d5b38de2
RA4Z <rraz639@gmail.com>
2023-11-22 16:26:21 -0300

    Estilizando e adicionando responsividade

--- src/pages/Trabalho/User/index.tsx
+++ src/pages/Trabalho/User/index.tsx
@@ -1,34 +1,30 @@
+import { info_especialistas } from 'pages/Pesquisa/infos'
 import styles from './User.module.scss'
 import Estrela from 'images/estrela.svg'
+import Button from 'components/Button'
 
 interface Props {
     visible: any,
-    titulo: string,
-    estrelas: string,
-    imagem: string,
-    cargo: string,
-    descricao: string,
+    id: number
 }
 
 export default function User(props: Props) {
+    var candidato = info_especialistas.filter(especialista => especialista.id === props.id)
     return (
         <>
             <div className={styles.overlay} onClick={() => props.visible(false)} />
             <div className={styles.container}>
-                <div className={styles.info}>
-                    <div className={styles.info__titulo}>{props.titulo}</div>
-                    <div className={styles.info__desc}>
-
-                        <div className={styles.textos}>
-                            <img src={Estrela} alt='Classificação em estrelas' />
-                            {props.estrelas}
-                        </div>
-                        <img src={props.imagem} alt='Imagem de perfil do usuário' className={styles.logotipo} />
-                        <div className={styles.textos}>{props.cargo}</div>
-
+                <div className={styles.info__titulo}>{candidato[0].titulo}</div>
+                <div className={styles.info__desc}>
+                    <div className={styles.textos}>
+                        <img src={Estrela} alt='Classificação em estrelas' />
+                        {candidato[0].estrelas}
                     </div>
-                    <p>{props.descricao}</p>
+                    <img src={candidato[0].imagem} alt='Imagem de perfil do usuário' className={styles.logotipo} />
+                    <div className={styles.textos}>{candidato[0].cargo}</div>
+                    <div className={styles.textos}>{candidato[0].descricao}</div>
                 </div>
+                <Button texto='Aprovar candidato' dark={true} />
             </div>
         </>
     )
--- src/pages/Trabalho/index.tsx
+++ src/pages/Trabalho/index.tsx
@@ -15,50 +15,56 @@ export default function Trabalho() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     const { userId, jobId } = useParams();
     const [userVisibility, setUserVisibility] = useState(false)
+    const [userSelecionado, setUserSelecionado] = useState(0)
     const navigate = useNavigate()
 
     var info = info_servicos.filter(service => service.idSolicitante === Number(userId) && service.id === Number(jobId))
     var inscritos = info_especialistas.filter(especialista => info[0].inscritos.indexOf(especialista.id) > -1)
+
+    function infoUsuarios(idUser: number) {
+        setUserSelecionado(idUser)
+        setUserVisibility(true)
+    }
 
     const userVisible = (childdata: boolean) => {
         setUserVisibility(childdata)
     }
 
     return (
-        <div className={styles.container}>
-            {userVisibility ?
-                <User
-                    visible={userVisible}
-                    titulo={'string'}
-                    estrelas={'string'}
-                    imagem={''}
-                    cargo={'string'}
-                    descricao={'string'}
-                />
-                : ''}
-            <img src={Voltar} alt='Seta para retornar à página anterior' onClick={() => navigate(-1)} className={styles.seta_volta} />
-            <div className={styles.info}>
-                <div className={styles.info__titulo}>{info[0].titulo}</div>
-                <div className={styles.info__desc}>
-                    <img src={Obra} alt='Imagem do serviço solicitado' className={styles.info__desc__serviceImg} />
-                    <p>{info[0].descricao} registrado para início em {info[0].diaProcurado} às {info[0].horarioProcurado}. Trabalho exclusivo para {info[0].necessario}, solicitado por {info[0].solicitadoPor}.</p>
-                </div>
+        <>
+            <div className={styles.pagina_overlay}>
+                {userVisibility ?
+                    <User
+                        visible={userVisible}
+                        id={userSelecionado}
+                    />
+                    : ''}
             </div>
-            <Divider />
-            <div>
-                <div className={styles.especialistas__title}>Especialistas Candidatados</div>
-                <div className={styles.especialistas__cards}>
-                    {inscritos.map(inscrito => (
-                        <div className={styles.especialistas__card} key={inscrito.id}>
-                            <img src={UserIMG} alt='Perfil de usuário' />
-                            <div>
-                                <p>{inscrito.titulo}</p>
-                                <p><img src={Estrela} alt='Estrela' />{inscrito.estrelas}</p>
+            <div className={styles.container}>
+                <img src={Voltar} alt='Seta para retornar à página anterior' onClick={() => navigate(-1)} className={styles.seta_volta} />
+                <div className={styles.info}>
+                    <div className={styles.info__titulo}>{info[0].titulo}</div>
+                    <div className={styles.info__desc}>
+                        <img src={Obra} alt='Imagem do serviço solicitado' className={styles.info__desc__serviceImg} />
+                        <p>{info[0].descricao} registrado para início em {info[0].diaProcurado} às {info[0].horarioProcurado}. Trabalho exclusivo para {info[0].necessario}, solicitado por {info[0].solicitadoPor}.</p>
+                    </div>
+                </div>
+                <Divider />
+                <div>
+                    <div className={styles.especialistas__title}>Especialistas Candidatados</div>
+                    <div className={styles.especialistas__cards}>
+                        {inscritos.map(inscrito => (
+                            <div className={styles.especialistas__card} key={inscrito.id} onClick={() => infoUsuarios(inscrito.id)}>
+                                <img src={UserIMG} alt='Perfil de usuário' />
+                                <div>
+                                    <p>{inscrito.titulo}</p>
+                                    <p><img src={Estrela} alt='Estrela' />{inscrito.estrelas}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
