ed91d292ac5cfb7e8e7713c6654d5b7758c79fe0
RA4Z <rraz639@gmail.com>
2023-11-22 16:54:32 -0300

    Instalando Firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB3pN2a8xZEbxmhpZ-XVcJYJ-jsh87ut5g",
+    authDomain: "server-of-business.firebaseapp.com",
+    projectId: "server-of-business",
+    storageBucket: "server-of-business.appspot.com",
+    messagingSenderId: "272108888489",
+    appId: "1:272108888489:web:581e39b679b9d3cb9424b8",
+    measurementId: "G-JJCYGM15HJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Inicializa o Firestore
+const db = getFirestore(app);
+
+const storage = getStorage(app)
+
+export { db, storage };
