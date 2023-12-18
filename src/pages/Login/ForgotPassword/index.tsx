22f18c535d8ff0c48b2c4b8127790a0aac6bc30c
RA4Z <rraz639@gmail.com>
2023-12-18 14:02:59 -0300

    Enviando E-mail para redefinição de senha

--- src/pages/Login/ForgotPassword/index.tsx
+++ src/pages/Login/ForgotPassword/index.tsx
@@ -1,9 +1,22 @@
-import { TextField } from '@mui/material'
+import { Snackbar, TextField } from '@mui/material'
 import styles from './ForgotPassword.module.scss'
 import Logo from 'images/logo.svg'
 import Button from 'components/Button'
+import { emailRedefinirSenha } from 'services/requisitions'
+import { useState } from 'react'
 
 export default function ForgotPassword({ visible, childToParent }: any) {
+    const [email, setEmail] = useState('')
+    const [statusToast, setStatusToast] = useState({ visivel: false, message: '' })
+
+    async function enviarProtocolo() {
+        const result = await emailRedefinirSenha(email)
+        if (result) {
+            setStatusToast({ visivel: true, message: 'Iremos enviar o protocolo de redefinição de senha ao seu endereço de E-mail' })
+        } else {
+            setStatusToast({ visivel: true, message: 'Ocorreu algum erro, tente novamente mais tarde!' })
+        }
+    }
     return (
         <>
             {visible ?
@@ -12,13 +25,22 @@ export default function ForgotPassword({ visible, childToParent }: any) {
                     <div className={styles.container}>
                         <img src={Logo} alt='Logotipo Server of Business' />
                         <p>Insira seu E-mail abaixo</p>
-                        <TextField id="outlined-basic" label="E-mail" variant="outlined" autoComplete="email" className={styles.input} />
+                        <TextField id="outlined-email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            label="E-mail" variant="outlined" autoComplete="email" className={styles.input} />
                         <p>Iremos enviar o protocolo de redefinição de senha</p>
                         <div className={styles.container__buttons}>
                             <Button dark={false} texto='Cancelar' onClick={() => childToParent(false)} />
-                            <Button dark={true} texto='Enviar Protocolo de Redefinição' />
+                            <Button dark={true} texto='Enviar Protocolo de Redefinição' onClick={() => enviarProtocolo()} />
                         </div>
                     </div>
+                    <Snackbar
+                        open={statusToast.visivel}
+                        onClose={() => setStatusToast({ ...statusToast, visivel: false })}
+                        autoHideDuration={3000}
+                        message={statusToast.message}
+                    />
                 </>
                 : ''}
         </>
--- src/services/requisitions.ts
+++ src/services/requisitions.ts
@@ -1,27 +1,27 @@
 import { auth } from "config/firebase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, AuthErrorCodes, sendPasswordResetEmail } from "firebase/auth";
 
 
-function errosFirebase(error:any) {
-    let mensagem = '';
-    switch(error.code) {
-        case AuthErrorCodes.EMAIL_EXISTS:
-            mensagem = "Esse E-mai já está em uso"
-            break;
-        case AuthErrorCodes.INVALID_EMAIL:
-            mensagem = "E-mail inválido"
-            break;
-        case AuthErrorCodes.WEAK_PASSWORD:
-            mensagem = "A senha precisa de no mínimo 6 caracteres"
-            break;
-        default:
-            mensagem="Erro desconhecido"
-    }
-    return mensagem
+function errosFirebase(error: any) {
+  let mensagem = '';
+  switch (error.code) {
+    case AuthErrorCodes.EMAIL_EXISTS:
+      mensagem = "Esse E-mai já está em uso"
+      break;
+    case AuthErrorCodes.INVALID_EMAIL:
+      mensagem = "E-mail inválido"
+      break;
+    case AuthErrorCodes.WEAK_PASSWORD:
+      mensagem = "A senha precisa de no mínimo 6 caracteres"
+      break;
+    default:
+      mensagem = "Erro desconhecido"
+  }
+  return mensagem
 }
 
-export async function cadastrar(email:string, senha:string) {
-    const resultado = await createUserWithEmailAndPassword(auth, email, senha)
+export async function cadastrar(email: string, senha: string) {
+  const resultado = await createUserWithEmailAndPassword(auth, email, senha)
     .then((dadosDoUsuario) => {
       console.log(dadosDoUsuario)
       return "sucesso"
@@ -30,19 +30,33 @@ export async function cadastrar(email:string, senha:string) {
       console.log(error)
       return errosFirebase(error)
     });
-    return resultado;
+  return resultado;
 }
 
-export async function logar(email:string, senha:string) {
-    const resultado = await signInWithEmailAndPassword(auth, email, senha)
+export async function logar(email: string, senha: string) {
+  const resultado = await signInWithEmailAndPassword(auth, email, senha)
     .then((dadosDoUsuario) => {
       console.log(dadosDoUsuario)
       return "sucesso"
     })
     .catch((error) => {
       console.log(error)
-      //return errosFirebase(error)
       return "erro"
     });
-    return resultado;
+  return resultado;
+}
+
+export async function emailRedefinirSenha(email: string) {
+  const result = await sendPasswordResetEmail(auth, email)
+    .then(() => {
+      return true
+    })
+    .catch(() => {
+      return false
+    });
+  if (result) {
+    return true
+  } else {
+    return false
+  }
 }
