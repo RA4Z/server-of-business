a9d3f731c075f3531578faacaec581d0579d648d
RA4Z <rraz639@gmail.com>
2024-01-10 09:43:09 -0300

    Melhorando menu de seleção

--- src/components/OptionMenu/index.tsx
+++ src/components/OptionMenu/index.tsx
@@ -6,6 +6,7 @@ import { useRef, useEffect, memo } from "react";
 
 import { signOut } from 'firebase/auth';
 import { auth } from 'config/firebase';
+import { Dialog, DialogContent, DialogTitle } from '@mui/material';
 
 interface Props {
     logado: boolean,
@@ -46,26 +47,37 @@ function OptionMenu(props: Props) {
     }
 
     return (
-        <div ref={wrapperRef} className={styles.container}>
-            <div className={styles.titulo}>Informações Adicionais</div>
-            {props.logado ?
-                <>
-                    {/* <Button texto='Tornar-se Premium' dark={false} onClick={() => navegar('/planos')} /> */}
-                    <Button texto='Procurar Especialistas' dark={false} onClick={() => navegar('/pesquisa/1')} />
-                    <Button texto='Solicitações em aberto' dark={false} onClick={() => navegar('/pesquisa/2')} />
-                    <Button texto='Perfil de Usuário' dark={false} onClick={() => navegar('/perfil')} />
-                    <Button texto='Logout' onClick={() => handleLogout()} dark={true} />
-                </>
-                :
-                <>
-                    <Button texto='Crie sua conta' dark={true} onClick={() => navegar('/cadastro')} />
-                    <Divider style={{ background: '#7C7C7C', width: '100%', margin: 5 }}></Divider>
-                    <Button texto='Especialistas Cadastrados' dark={false} onClick={() => navegar('/pesquisa/1')} />
-                    <Button texto='Serviços Solicitados em aberto' dark={false} onClick={() => navegar('/pesquisa/2')} />
-                    <Button texto='Fazer Login' dark={false} onClick={() => navegar('/login')} />
-                </>
-            }
-        </div>
+        <>
+            <Dialog
+                open={props.mostrarOption}
+                onClose={() => props.mostrarOption(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                className={styles.background}>
+                <DialogTitle id="alert-dialog-title" style={{ background: '#e8e7e7' }}>
+                    {`Informações Adicionais`}
+                </DialogTitle>
+                <DialogContent className={styles.container}>
+                    {props.logado ?
+                        <>
+                            {/* <Button texto='Tornar-se Premium' dark={false} onClick={() => navegar('/planos')} /> */}
+                            <Button texto='Procurar Especialistas' dark={false} onClick={() => navegar('/pesquisa/1')} />
+                            <Button texto='Solicitações em aberto' dark={false} onClick={() => navegar('/pesquisa/2')} />
+                            <Button texto='Perfil de Usuário' dark={false} onClick={() => navegar('/perfil')} />
+                            <Button texto='Logout' onClick={() => handleLogout()} dark={true} />
+                        </>
+                        :
+                        <>
+                            <Button texto='Crie sua conta' dark={true} onClick={() => navegar('/cadastro')} />
+                            <Divider style={{ background: '#7C7C7C', width: '100%', margin: 5 }}></Divider>
+                            <Button texto='Especialistas Cadastrados' dark={false} onClick={() => navegar('/pesquisa/1')} />
+                            <Button texto='Serviços Solicitados em aberto' dark={false} onClick={() => navegar('/pesquisa/2')} />
+                            <Button texto='Fazer Login' dark={false} onClick={() => navegar('/login')} />
+                        </>
+                    }
+                </DialogContent>
+            </Dialog>
+        </>
     )
 }
 export default memo(OptionMenu)
